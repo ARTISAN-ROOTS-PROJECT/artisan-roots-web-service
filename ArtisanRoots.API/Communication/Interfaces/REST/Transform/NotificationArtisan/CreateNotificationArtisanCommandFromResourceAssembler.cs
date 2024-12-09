43abc8890c59a5fc5cd07a31b7d5b4c89db000a0
Mauricio <mauriciorojasanchez@gmail.com>
2024-12-09 00:55:35 -0500

    feat(communication): added transform for notification artisan aggregate.

---
+++ ArtisanRoots.API/Communication/Interfaces/REST/Transform/NotificationArtisan/CreateNotificationArtisanCommandFromResourceAssembler.cs
@@ -0,0 +1,12 @@
+﻿using ArtisanRoots.API.Communication.Domain.Model.Commands;
+using ArtisanRoots.API.Communication.Interfaces.REST.Resources.NotificationArtisan;
+
+namespace ArtisanRoots.API.Communication.Interfaces.REST.Transform.NotificationArtisan;
+
+public static class CreateNotificationArtisanCommandFromResourceAssembler
+{
+    public static CreateNotificationArtisanCommand ToCommandFromResorce(CreateNotificationArtisanResource resource)
+    {
+        return new CreateNotificationArtisanCommand(resource.Title, resource.Content, resource.ArtisansId);
+    }
+}
---
+++ ArtisanRoots.API/Communication/Interfaces/REST/Transform/NotificationArtisan/NotificationArtisanResourceFromEntityAssembler.cs
@@ -0,0 +1,13 @@
+﻿using ArtisanRoots.API.Communication.Domain.Model.Aggregates;
+using ArtisanRoots.API.Communication.Interfaces.REST.Resources.NotificationArtisan;
+
+namespace ArtisanRoots.API.Communication.Interfaces.REST.Transform.NotificationArtisan;
+
+public static class NotificationArtisanResourceFromEntityAssembler
+{
+    public static NotificationArtisanResource ToResourceFromEntity(Domain.Model.Aggregates.NotificationArtisan entity)
+    {
+        return new NotificationArtisanResource(entity.Id, entity.Title, entity.Content, entity.ArtisansId);
+    }
+}
+

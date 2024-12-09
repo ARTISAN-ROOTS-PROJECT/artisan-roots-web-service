94db84fc86028c0f8c3089047dd58afaf66003ed
Mauricio <mauriciorojasanchez@gmail.com>
2024-12-09 00:54:59 -0500

    feat(communication): added resources for aggregate notification owner.

---
+++ ArtisanRoots.API/Communication/Interfaces/REST/Resources/NotificationOwner/CreateNotificationOwnerResource.cs
@@ -0,0 +1,3 @@
+﻿namespace ArtisanRoots.API.Communication.Interfaces.REST.Resources.NotificationOwner;
+
+public record CreateNotificationOwnerResource(string Title, string Content, int OwnersId);
---
+++ ArtisanRoots.API/Communication/Interfaces/REST/Resources/NotificationOwner/NotificationOwnerResource.cs
@@ -0,0 +1,3 @@
+﻿namespace ArtisanRoots.API.Communication.Interfaces.REST.Resources.NotificationOwner;
+
+public record NotificationOwnerResource(int Id, string? Title, string? Content, int ArtisansId);

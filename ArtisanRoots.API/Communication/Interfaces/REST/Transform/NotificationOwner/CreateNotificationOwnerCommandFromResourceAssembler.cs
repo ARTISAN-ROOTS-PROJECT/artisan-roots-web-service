fdbda5fb929bb976ffac5971f978d551570b4c02
Mauricio <mauriciorojasanchez@gmail.com>
2024-12-09 00:56:05 -0500

    feat(communication): added transform for notification owner aggregate.

---
+++ ArtisanRoots.API/Communication/Interfaces/REST/Transform/NotificationOwner/CreateNotificationOwnerCommandFromResourceAssembler.cs
@@ -0,0 +1,13 @@
+﻿using ArtisanRoots.API.Communication.Domain.Model.Commands;
+using ArtisanRoots.API.Communication.Interfaces.REST.Resources.NotificationOwner;
+
+namespace ArtisanRoots.API.Communication.Interfaces.REST.Transform.NotificationOwner
+{
+    public static class CreateNotificationOwnerCommandFromResourceAssembler
+    {
+        public static CreateNotificationOwnerCommand ToCommandFromResource(CreateNotificationOwnerResource resource)
+        {
+            return new(resource.Title, resource.Content, resource.OwnersId);
+        }
+    }
+}
---
+++ ArtisanRoots.API/Communication/Interfaces/REST/Transform/NotificationOwner/NotificationOwnerResourceFromEntityAssembler.cs
@@ -0,0 +1,13 @@
+﻿using ArtisanRoots.API.Communication.Domain.Model.Aggregates;
+using ArtisanRoots.API.Communication.Interfaces.REST.Resources.NotificationOwner;
+
+namespace ArtisanRoots.API.Communication.Interfaces.REST.Transform.NotificationOwner;
+
+public static class NotificationOwnerResourceFromEntityAssembler
+{
+    public static NotificationOwnerResource ToResourceFromEntity(Domain.Model.Aggregates.NotificationOwner entity)
+    {
+        return new NotificationOwnerResource(entity.Id, entity.Title, entity.Content, entity.OwnersId);
+    }
+}
+   

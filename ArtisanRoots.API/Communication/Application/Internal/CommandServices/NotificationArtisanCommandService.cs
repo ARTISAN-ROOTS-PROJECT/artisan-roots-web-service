4ad9abecbf52daf699fa0ae97592cdea9ca7e66c
Mauricio <mauriciorojasanchez@gmail.com>
2024-12-08 22:22:53 -0500

    feat(communication): added command service for notification artisan aggregate.

--- ArtisanRoots.API/Communication/Application/Internal/CommandServices/NotificationArtisanCommandService.cs
+++ ArtisanRoots.API/Communication/Application/Internal/CommandServices/NotificationArtisanCommandService.cs
@@ -1,15 +1,27 @@
 ﻿using ArtisanRoots.API.Communication.Domain.Model.Aggregates;
 using ArtisanRoots.API.Communication.Domain.Model.Commands;
+using ArtisanRoots.API.Communication.Domain.Model.Exceptions;
+using ArtisanRoots.API.Communication.Domain.Repositories;
 using ArtisanRoots.API.Communication.Domain.Services.CommandServices;
+using ArtisanRoots.API.Shared.Domain.Repositories;
 
 namespace ArtisanRoots.API.Communication.Application.Internal.CommandServices;
 
-public class NotificationArtisanCommandService : INotificationArtisanCommandService
+public class NotificationArtisanCommandService(IUnitOfWork unitOfWork, 
+    INotificationArtisanRepository notificationArtisanRepository) : INotificationArtisanCommandService
 {
     public async Task<NotificationArtisan?> Handle(CreateNotificationArtisanCommand command)
     {
-        if(string.IsNullOrEmpty(command.Title) || string.IsNullOrEmpty(command.Content) || command.ArtisansId == 0)
-            
+        if (string.IsNullOrEmpty(command.Title) || string.IsNullOrEmpty(command.Content) || command.ArtisansId == 0)
+            throw new AnyAttributeCanBeEmptyOrNullException();
+
+        var entity = new NotificationArtisan(command);
+
+        await notificationArtisanRepository.AddAsync(entity);
+
+        await unitOfWork.CommitAsync();
+
+        return entity;
     }
 }
 

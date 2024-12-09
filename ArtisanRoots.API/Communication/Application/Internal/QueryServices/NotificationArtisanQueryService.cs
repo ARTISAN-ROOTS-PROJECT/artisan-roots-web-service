00336850efd0df4df11df147f2ad823319da9276
Mauricio <mauriciorojasanchez@gmail.com>
2024-12-08 22:57:35 -0500

    feat(communciation): added query service for aggregate notification artisan.

---
+++ ArtisanRoots.API/Communication/Application/Internal/QueryServices/NotificationArtisanQueryService.cs
@@ -0,0 +1,13 @@
+﻿using ArtisanRoots.API.Communication.Domain.Model.Aggregates;
+using ArtisanRoots.API.Communication.Domain.Model.Queries;
+using ArtisanRoots.API.Communication.Domain.Repositories;
+using ArtisanRoots.API.Communication.Domain.Services.QueryServices;
+
+namespace ArtisanRoots.API.Communication.Application.Internal.QueryServices;
+
+public class NotificationArtisanQueryService(INotificationArtisanRepository notificationArtisanRepository) : INotificationArtisanQueryService
+{
+    public async Task<IEnumerable<NotificationArtisan>> Handle(GetAllNotificationsQuery query)
+    => await notificationArtisanRepository.ListAsync();
+
+}

fd649c898c47161991fac49cfecfdbeb35cb0fd9
Mauricio <mauriciorojasanchez@gmail.com>
2024-12-08 22:55:19 -0500

    feat(communication): added command service for aggregate notification owner.

---
+++ ArtisanRoots.API/Communication/Application/Internal/CommandServices/NotificationOwnerCommandService.cs
@@ -0,0 +1,27 @@
+﻿using ArtisanRoots.API.Communication.Domain.Model.Aggregates;
+using ArtisanRoots.API.Communication.Domain.Model.Commands;
+using ArtisanRoots.API.Communication.Domain.Model.Exceptions;
+using ArtisanRoots.API.Communication.Domain.Repositories;
+using ArtisanRoots.API.Communication.Domain.Services.CommandServices;
+using ArtisanRoots.API.Shared.Domain.Repositories;
+
+namespace ArtisanRoots.API.Communication.Application.Internal.CommandServices;
+
+public class NotificationOwnerCommandService(INotificationOwnerRepository notificationOwnerRepository, IUnitOfWork unitOfWork) : INotificationOwnerCommandService
+{
+    public async Task<NotificationOwner?> Handle(CreateNotificationOwnerCommand command)
+    {
+        if (string.IsNullOrEmpty(command.Title) || string.IsNullOrEmpty(command.Content) || command.OwnersId == 0)
+            throw new AnyAttributeCanBeEmptyOrNullException();
+
+        var entity = new NotificationOwner(command);
+
+        await notificationOwnerRepository.AddAsync(entity);
+
+        await unitOfWork.CommitAsync();
+
+        return entity;
+    }
+
+}
+

865811ee8f8568a185956a8eab57a57b1113d986
Mauricio <mauriciorojasanchez@gmail.com>
2024-12-08 23:00:02 -0500

    feat(communication): added notification artisan repository.

--- ArtisanRoots.API/Communication/Domain/Repositories/INotificationArtisanRepository.cs
+++ ArtisanRoots.API/Communication/Domain/Repositories/INotificationArtisanRepository.cs
@@ -4,7 +4,6 @@ using ArtisanRoots.API.Shared.Domain.Repositories;
 namespace ArtisanRoots.API.Communication.Domain.Repositories;
     
 public interface INotificationArtisanRepository : IBaseRepository<NotificationArtisan>
-{
-    Task<IEnumerable<NotificationArtisan>> FindAllAsync();
+{ 
 
 }

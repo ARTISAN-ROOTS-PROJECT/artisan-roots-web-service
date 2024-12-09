4f214be457b60ef98d65992efddc7c5682ae4350
Mauricio <mauriciorojasanchez@gmail.com>
2024-12-08 22:58:05 -0500

    feat(communication): added query service for aggregate notification owner.

---
+++ ArtisanRoots.API/Communication/Application/Internal/QueryServices/NotificationOwnerQueryService.cs
@@ -0,0 +1,14 @@
+﻿using ArtisanRoots.API.Communication.Domain.Model.Aggregates;
+using ArtisanRoots.API.Communication.Domain.Model.Queries;
+using ArtisanRoots.API.Communication.Domain.Repositories;
+using ArtisanRoots.API.Communication.Domain.Services.QueryServices;
+
+namespace ArtisanRoots.API.Communication.Application.Internal.QueryServices;
+
+public class NotificationOwnerQueryService(INotificationOwnerRepository notificationOwnerRepository) : INotificationOwnerQueryService
+{
+    public async Task<IEnumerable<NotificationOwner>> Handle(GetAllNotificationsQuery query)
+    => await notificationOwnerRepository.ListAsync();
+
+}
+
